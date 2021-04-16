76ece13d46a8903c7a4864d35667528cbb2381c9
Rishi Agrawal <rishiagrawal2609@gmail.com>
2021-04-16 17:00:36 +0530

    added missing things

--- Django_Bootstrap1/G/settings.py
+++ Django_Bootstrap1/G/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w^tr=ce%kra7@m+tt=1%6a@tz*ohe2db@hnxeu-7pm66^x90@$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
  
-ALLOWED_HOSTS = [13.126.113.73]
+ALLOWED_HOSTS = ['13.126.113.73']
 
 
 # Application definition
